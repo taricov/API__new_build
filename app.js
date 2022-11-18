31f76fee55fd31baf2a4e928eb76a26d7a585d9e
TaricOv <tareq.ouf@gmail.com>
2022-11-18 03:07:31 +0200

    new commit

--- app.js
+++ app.js
@@ -2,18 +2,26 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
-const { format } = require("path")
+const dotenv = require("dotenv")
+const path = require("path")
+
+
+dotenv.config({ path: "./config/config.env" })
+const db = require("./config/db")
+
+db()
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cors())
+app.use("/static", express.static(path.join(__dirname, "public")))
 
 app.get('/', (req, res) => {
     res.send("Iam it!")
 })
 
-
-app.listen(10000,
-    () => console.log("fuck u!!"))
+app.listen(process.env.PORT, () => {
+    console.log("server is here!!!!")
+})
--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDatabase = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log("fffff")
+    console.log(conn)
 }
 
 
